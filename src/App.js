9a67dc9a730df1f4f9f04e49a0c113374a5195fe
victor <victorblanco281201@gmail.com>
2022-11-13 13:28:52 -0400

    Mi primer commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect,useContext } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './context/auth-context';
+
+function App() {
+  const ctx=useContext(AuthContext);
+  /*const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+
+  useEffect(()=>{
+    const storeUserInfo=localStorage.getItem('isLoggedIn');
+    if(storeUserInfo==='1'){
+      setIsLoggedIn(true);
+    }
+  },[]);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+   
+    localStorage.setItem('isLoggedIn','1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };*/
+
+  return (
+  
+      <React.Fragment>
+
+      
+      <MainHeader/>
+      <main>
+        {!ctx.isLoggedIn && <Login  />}
+        {ctx.isLoggedIn && <Home  />}
+      </main>
+     </React.Fragment>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, {useContext} from 'react';
+import AuthContext from '../../context/auth-context';
+
+import Card from '../UI/Card/Card';
+import classes from './Home.module.css';
+
+const Home = (props) => {
+  const authCtx=useContext(AuthContext);
+  return (
+    <Card className={classes.home}>
+      <h1>Welcome back!</h1>
+    </Card>
+  );
+};
+
+export default Home;
