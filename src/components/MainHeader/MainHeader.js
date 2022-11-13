cb3d4e42559d71691afd3f04333882ffc04077cf
victor <victorblanco281201@gmail.com>
2022-11-13 17:02:39 -0400

    ahora pruebo en otro branch

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
+      <h1>A Typical Page, ando en el branch de lewis</h1>
       <Navigation/>
     </header>
   );
